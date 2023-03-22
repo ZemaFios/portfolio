daa650627a3945962247b7f65818ba19ac0493ac
Søren Nielsen <sorennielsen13@hotmail.com>
2023-03-22 13:22:48 +0100

    feat: add dynamic projectsdata

---
+++ scripts/projectsData.js
@@ -0,0 +1,36 @@
+const projectsData = [
+  {
+    title: 'GOURMET CLUB - Le goût infini',
+    image: 'img/gourmet-club.png',
+    imageAltText: 'Gourmet club',
+    projectLink: 'https://zemafios.github.io/Gourmet-club/',
+    codeLink: 'https://github.com/ZemaFios/Gourmet-club',
+    undertext: [{ html: 'HTML', css: 'CSS', js: 'JavaScript' }],
+  },
+  {
+    title: 'Kasper Eistrup',
+    image: 'img/eistrup.png',
+    imageAltText: 'Kasper Eistrup',
+    projectLink: 'https://zemafios.github.io/Kasper-eistrup/',
+    codeLink: 'https://github.com/ZemaFios/Kasper-eistrup',
+    undertext: [{ html: 'HTML', css: 'CSS', js: 'JavaScript' }],
+  },
+  {
+    title: 'Sushi Royal',
+    image: 'img/sushi-royal.png',
+    imageAltText: 'Sushi Royal',
+    projectLink: 'https://zemafios.github.io/Sushi-royal/',
+    codeLink: 'https://github.com/ZemaFios/Sushi-royal',
+    undertext: [{ html: 'HTML', css: 'CSS', js: 'JavaScript' }],
+  },
+  {
+    title: 'Cryptana Invest',
+    image: 'img/cryptana-invest.png',
+    imageAltText: 'Cryptana Invest',
+    projectLink: 'https://zemafios.github.io/Cryptana-invest/',
+    codeLink: 'https://github.com/ZemaFios/Cryptana-invest',
+    undertext: [{ html: 'HTML', css: 'CSS', js: 'JavaScript' }],
+  },
+];
+
+export default projectsData;
