a84058062e7705d37c3a8dcbc1d722042145f94c
Søren Nielsen <sorennielsen13@hotmail.com>
2023-03-22 13:20:24 +0100

    feat: export navigation into index

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,12 +1,14 @@
-const hamburger = document.querySelector('.hamburger');
-const navList = document.querySelector('.nav-list');
+export const navigation = () => {
+    const hamburger = document.querySelector('.hamburger');
+    const navList = document.querySelector('.nav-list');
 
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    navList.classList.toggle('active');
-});
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        navList.classList.toggle('active');
+    });
 
-navList.addEventListener('click', ()  => {
-    hamburger.classList.remove('active');
-    navList.classList.remove('active');
-})
+    navList.addEventListener('click', ()  => {
+        hamburger.classList.remove('active');
+        navList.classList.remove('active');
+    })
+};
